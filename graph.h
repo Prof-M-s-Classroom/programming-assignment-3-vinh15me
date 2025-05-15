c835d191c257be62243812af3ac1462912798867
Vinh Tran <vinh15me@outlook.com>
2025-05-14 17:57:12 -0700

    whoops
    I did this in a wrong way and I got to fix it.

--- graph.h
+++ graph.h
@@ -1,12 +1,51 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+#include "heap.h"
 
 class Graph {
 public:
-    Graph(int vertices);
-    ~Graph();
-    void addEdge(int u, int v, int weight);
-    void primMST();  // Must print MST edges and total weight
+    Graph(int vertices): numVertices(vertices) {
+        adjMatrix = new int *[vertices];
+        for (int i = 0; i < vertices; ++i) {
+            adjMatrix[i] = new int[vertices];
+        }
+        for (int i = 0; i < vertices; ++i) {
+            for (int j = 0; j < vertices; ++j) {
+                adjMatrix[i][j] = INT_MAX;
+                if (i == j) {
+                    adjMatrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    ~Graph() {
+        delete[] adjMatrix;
+    }
+
+    void addEdge(int u, int v, int weight) {
+        adjMatrix[u][v] = weight;
+        adjMatrix[v][u] = weight;
+    }
+    void primMST() {
+
+
+
+        MinHeap* min_heap = new MinHeap(numVertices);
+        for (int x = 0; x < numVertices; ++x) {
+            for (int y = x+1; y < numVertices; ++y) {
+                min_heap->decreaseKey(y,adjMatrix[x][y],x);
+            }
+        }
+
+        printf("\n");
+        min_heap->print();
+
+    };  // Must print MST edges and total weight
+
+    int** getAdjMatrix() {
+        return adjMatrix;
+    }
 
 private:
     int** adjMatrix;
--- heap.h
+++ heap.h
@@ -1,24 +1,58 @@
 #ifndef HEAP_H
 #define HEAP_H
+#include <ranges>
 
 class MinHeap {
 public:
-    MinHeap(int capacity);
+    MinHeap(int capacity): capacity(capacity) {
+        size = 0;
+        keyArray = new int[capacity];
+        heapArray = new int[capacity];
+        position = new int[capacity];
+        for (int i = 0; i < capacity; i++) {
+            position[i] = -1;
+            keyArray[i] = INT_MAX;
+            heapArray[i] = i;
+        }
+        keyArray[0] = 0;
+    }
     ~MinHeap();
-    void insert(int vertex, int key);
-    int extractMin();
-    void decreaseKey(int vertex, int newKey);
+    void insert(int vertex, int key){
+
+    }
+
+    int extractMin() {
+    }
+
+    void decreaseKey(int vertex, int newKey, int old) {
+        if (keyArray[vertex] > newKey){
+        keyArray[vertex] = newKey;
+        position[vertex] = old;
+        }
+    }
+
     bool isInMinHeap(int vertex);
-    bool isEmpty();
+    bool isEmpty() {
+        return size == 0.;
+    }
+
+    void minHeapify(int idx) {
+    }
+
+
+    void print() {
+        for (int i = 1; i < capacity; i++) {
+            printf("%d -- %d (%d)\n", position[i], i,keyArray[i]);
+        }
+    }
 
 private:
     int* heapArray;        // Heap of vertex indices
-    int* keyArray;         // Corresponding key values
+    int* keyArray;         // Corresponding weight values to other vertices
     int* position;         // Maps vertex to its position in heap
     int capacity;
     int size;
 
-    void minHeapify(int idx);
 };
 
 #endif
